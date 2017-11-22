530717b099732907457c5baec578d7b8a1007669
Marenko Babic <marinko_@hotmail.com>
2017-11-22 15:51:39 +0100

    aa

---
+++ PersonenSuchen/Program.cs
@@ -0,0 +1,42 @@
+﻿
+namespace PersonenSuchen
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    enum Haarfarbe { Rot, Blau, Grün, Blond, Schwarz, Braun }
+
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            RandomName Name = new RandomName();
+            List<RandomName> list = new List<RandomName>();
+            Console.WriteLine( "Guten Tag" );
+            Console.WriteLine( "Wieviele Personen Personen wollen sie erstellen" );
+            bool eingabe = int.TryParse( Console.ReadLine(), out int result );
+            for( int i = 0; i < result; i++ )
+            {
+                Name.FirstName();
+                Name.Lastname();
+                Name.Date();
+                list.Add( Name );
+            }
+            foreach( var item in list )
+            {
+                Console.WriteLine( item );
+            }
+            Console.WriteLine();
+            Console.WriteLine( "Person suchen" );
+            string personenSuche = Console.ReadLine();
+
+            var findPerson = list.Where( x =>  x.FirstName);
+
+
+            Console.WriteLine(personenSuche);
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ PersonenSuchen/RandomName.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PersonenSuchen
+{
+    class RandomName
+    {
+        Random rnd = new Random();
+        public DateTime Date()
+        {
+            int year = rnd.Next( 1990, 2017 );
+            int month = rnd.Next( 1, 12 );
+            int day = DateTime.DaysInMonth( year, month );
+            int Day = rnd.Next( 1, day );
+
+            DateTime dt = new DateTime( year, month, Day );
+            return dt;
+        }
+        public String FirstName()
+        {
+            string firstName = firstNameList.OrderBy(a => rnd.NextDouble() ).First();
+            return firstName;
+        }
+        public String Lastname()
+        {
+            string lastName = lastNameList.OrderBy( a => rnd.NextDouble() ).First();
+            return lastName;
+        }
+        
+
+        private List<string> firstNameList = new List<string>()
+        { "Josef", "Sepp", "Hans", "Andi", "Peter", "Robert", "Marenko", "Patrick", "Thomas", "Stefan", "Patricia", "Michi", "Markus", "Steven" };
+        private List<string> lastNameList = new List<string>()
+        { "Prethaler", "Pichler", "Eiweck", "Wolfrath", "Ratzenböck", "Babic", "Grewe", "Gajar", "Bintinger", "Doe" };
+    }
+}
